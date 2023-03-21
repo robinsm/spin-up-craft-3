62d636cc10f84af7ecffa960e3d7cc2e4576e60d
Robin Mannering <robin@creative74.co.uk>
2023-03-21 08:49:18 +0100

    Initial commit

---
+++ .docker-config/php/run_queue.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Run Queue shell script
+#
+# This shell script runs the Craft CMS queue via `php craft queue/listen`
+# It waits until the database container responds, then runs any pending
+# migrations / project config changes via the `craft-update` Composer script,
+# then runs the queue listener that listens for and runs pending queue jobs
+#
+# @author    nystudio107
+# @copyright Copyright (c) 2023 nystudio107
+# @link      https://nystudio107.com/
+# @license   MIT
+
+cd /var/www/project
+# Wait until the MySQL db container responds
+echo "### Waiting for MySQL database"
+until eval "mysql -h mysql -u $CRAFT_DB_USER -p$CRAFT_DB_PASSWORD $CRAFT_DB_DATABASE -e 'select 1' > /dev/null 2>&1"
+do
+    sleep 1
+done
+# Wait until the `composer install` is done by looking for the `vendor/autoload.php` file
+echo "### Waiting for vendor/autoload.php"
+while [ ! -f vendor/autoload.php ]
+do
+  sleep 1
+done
+# Ensure permissions on directories Craft needs to write to
+chown -R www-data:www-data /var/www/project/storage
+chown -R www-data:www-data /var/www/project/web/cpresources
+# Create a security key if it doesn't exist already
+if [ -z "$CRAFT_SECURITY_KEY" ] ; then
+    su-exec www-data php craft setup/security-key --interactive=0
+fi
+# Create an app id if it doesn't exist already
+if [ -z "$CRAFT_APP_ID" ] ; then
+    su-exec www-data php craft setup/app-id --interactive=0
+fi
+# Run any pending migrations/project config changes
+su-exec www-data php craft up --interactive=0
+# Banner message
+source '/var/www/banner_message.sh'
+# Run a queue listener
+su-exec www-data php craft queue/listen 10
